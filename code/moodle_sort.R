e0f43fb9e6eaa235bae9d93a246b27a587fbb108
bdemeshev <boris.demeshev@gmail.com>
2021-11-16 22:36:36 +0300

    moodle sortirator v2

--- code/moodle_sort.R
+++ code/moodle_sort.R
@@ -1,9 +1,22 @@
 # moodle sorting
-# выгрузка
+# шаг 1. выгрузка прикрепленных ответов:
 # 1. клик по тесту
 # 2. клик по шестерёнке справа
 # 3. клик по "файлы ответов эссе"
 # 4. галочки не ставим
+# шаг 2. выгрузка фактических условий:
+# 1. клик по тесту
+# 2. клик по шестерёнке справа
+# 3. клик по "отчеты в pdf"
+# иногда глючит и говорит что нет какого-то плагина wkhtmlpdf
+# лечится чем-то типа выйти и снова зайти как положено
+# 4. сформировать отчёт "для вступительных"
+# ждём пару минут
+# 5. клик по скачать
+# шаг 3. разархивируем оба архива с шага 1 и шага 2 в общую папку.
+# шаг 4. в эту же папку скачиваем xlsx со списком студентов из гугл-дока
+# правим в скрипте setwd() на эту папку.
+
 
 library(tidyverse)
 library(rio)
@@ -37,7 +50,12 @@ for (i in 1:nrow(code_list)) {
   old_name = code_list$id[i]
   new_name = paste0(code_list$group[i], '/', code_list$student[i], '__', code_list$id[i])
 
-  cat('from:', old_name, 'to:', new_name, '\n')
+  cat('answers from:', old_name, 'to:', new_name, '\n')
+  file.rename(old_name, new_name)
+
+  old_name = paste0('no_asav_id_', code_list$id[i], '.pdf')
+  new_name = paste0(code_list$group[i], '/', code_list$student[i], '__', code_list$id[i], '/', code_list$student[i], '__', code_list$id[i], '__variant.pdf')
+  cat('variant from:', old_name, 'to:', new_name, '\n')
   file.rename(old_name, new_name)
 }
 
